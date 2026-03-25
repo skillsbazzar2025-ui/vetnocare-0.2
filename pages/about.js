04907a4f60f3765689eaf1aacb20a722b3771cbb
sj-mart-retails-1 <skillsbazzar2025@gmail.com>
2026-03-25 13:57:25 +0530

    Create about.js

---
+++ pages/about.js
@@ -0,0 +1,11 @@
+import Navbar from '../components/Navbar';
+
+export default function About() {
+  return (
+    <>
+      <Navbar />
+      <h1 style={{ textAlign: 'center', marginTop: '50px' }}>About Us</h1>
+      <p style={{ textAlign: 'center' }}>We care for your pets</p>
+    </>
+  );
+}
