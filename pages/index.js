4a518b11e4d47a2f3212da2237b7977715f108d2
sj-mart-retails-1 <skillsbazzar2025@gmail.com>
2026-03-25 13:57:04 +0530

    Create index.js

---
+++ pages/index.js
@@ -0,0 +1,11 @@
+import Navbar from '../components/Navbar';
+
+export default function Home() {
+  return (
+    <>
+      <Navbar />
+      <h1 style={{ textAlign: 'center', marginTop: '50px' }}>Welcome to Vetnocare</h1>
+      <p style={{ textAlign: 'center' }}>Pet Care Services</p>
+    </>
+  );
+}
