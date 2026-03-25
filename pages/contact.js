384eb32f54a0934e85819204c426f72c456754fd
sj-mart-retails-1 <skillsbazzar2025@gmail.com>
2026-03-25 13:57:44 +0530

    Create contact.js

---
+++ pages/contact.js
@@ -0,0 +1,11 @@
+import Navbar from '../components/Navbar';
+
+export default function Contact() {
+  return (
+    <>
+      <Navbar />
+      <h1 style={{ textAlign: 'center', marginTop: '50px' }}>Contact Us</h1>
+      <p style={{ textAlign: 'center' }}>Email: info@vetnocare.com</p>
+    </>
+  );
+}
