c47d5681c3e1b7603699f8e5201f175738aedd5d
sj-mart-retails-1 <skillsbazzar2025@gmail.com>
2026-03-25 11:52:10 +0530

    Update Footer.js

--- components/Footer.js
+++ components/Footer.js
@@ -1,1 +1,7 @@
-
+export default function Footer() {
+  return (
+    <footer style={{ textAlign: 'center', padding: '1rem', marginTop: '2rem', borderTop: '1px solid #1e2a3a' }}>
+      <p>© 2024 Vetnocare. All rights reserved.</p>
+    </footer>
+  );
+}
